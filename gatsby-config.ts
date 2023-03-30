b4bcc8458d747d9b4f9ba176fda835a7db8555a6
Alexey Khatskelevich <hatskelevich.alesha@gmail.com>
2023-03-31 01:39:34 +0500

    feat:  added manifest and favicon

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -8,7 +8,29 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [`gatsby-plugin-sass`],
+  plugins: [
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Alexey Khatskelevich",
+        short_name: "Alex Portfolio",
+        description: `Portfolio of a good guy, engineer and creator`,
+        lang: `en`,
+        start_url: "/",
+        background_color: "#f1f2f4",
+        theme_color: "#f1f2f4",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/images/favicons/icon.png", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
+      }
+    }
+  ],
 }
 
 export default config
